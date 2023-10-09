979842b90592a3176fd37a391024041a2d1e1a1c
Araceli GP <araceli-44@hotmail.com>
2023-10-09 12:30:00 +0100

    Create script of JS and if else show for btn menu

---
+++ resources/js/menu-mobile.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    
+    // Seleccionar mis dos elemntos principales
+
+    let mobile_btn = document.querySelector(".navbar__mobile-btn");
+    let mobile_menu = document.querySelector(".menu-mobile");
+
+    mobile_btn.addEventListener("click", () => {
+        let show = document.querySelector(".menu-mobile--show");
+
+        if(show){
+            mobile_menu.classList.remove("menu-mobile--show");
+        }else{
+            mobile_menu.classList.add("menu-mobile--show");
+        }
+    });
+});
